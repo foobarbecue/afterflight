edbd470de1040fe504ce5334bb135648a2586d7a
Aaron Curtis <aaron.curtis@cantab.net>
2012-10-29 08:08:07 -0600

    Wrote models and import script.

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'afterflight',                      # Or path to database file if using sqlite3.
+        'NAME': 'afterflight.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -115,6 +115,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'logbrowse'
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
@@ -149,3 +150,4 @@ LOGGING = {
         },
     }
 }
+PYMAVLINK_PATH='/home/aaron/arducopter/mavlink/pymavlink'
